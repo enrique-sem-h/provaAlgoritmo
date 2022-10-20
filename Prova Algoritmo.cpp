d418758361ed0db7a1f8b177915ace1b79d3f308
ENRIQUE DE LIMA CARVALHO <enrique.carvalho@a.ucb.br>
2022-10-20 12:53:01 -0300

    Questao 1

---
+++ Prova Algoritmo.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	printf("Nome da disciplina: Algoritmo e Programacao\nNome do professor: Joyce Siqueira\nNome completo do aluno: Enrique de Lima Carvalho\n");
+	printf("Matricula: UC22103250\nCurso: Engenharia de Software\nLink do Repositorio: https://github.com/enrique-sem-h/provaAlgoritmo\n");
+	printf("Software utilizado: Dev-C++\n");
+
+}
