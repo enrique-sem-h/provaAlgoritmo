db7f5586e2185824997c96c63ce262671b862c4c
ENRIQUE DE LIMA CARVALHO <enrique.carvalho@a.ucb.br>
2022-10-20 13:12:57 -0300

    Questao 2

--- Enrique Carvalho_UC22103250.cpp
+++ Enrique Carvalho_UC22103250.cpp
@@ -5,5 +5,24 @@ int main(){
 	printf("Nome da disciplina: Algoritmo e Programacao\nNome do professor: Joyce Siqueira\nNome completo do aluno: Enrique de Lima Carvalho\n");
 	printf("Matricula: UC22103250\nCurso: Engenharia de Software\nLink do Repositorio: https://github.com/enrique-sem-h/provaAlgoritmo\n");
 	printf("Software utilizado: Dev-C++\n");
+	
+	char opcaoMenu;
+	
+	printf("\nDigite a opcao desejada:\nA - Questao A\nB - Questao B\nC - Questao C\n");
+	scanf("%c", &opcaoMenu);
+	
+	switch(opcaoMenu)
+	{
+		case 'A':
+		
+			break;
+
+		case 'B':
+			
+			break;
+
+		default: 
+			printf("Opcao Invalida");
+	}
 
 }
